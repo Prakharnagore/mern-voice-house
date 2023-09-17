8bc2455236a384f02c85276b5ecfa195726c140d
Prakhar Nagore <prakharnagore111@gmail.com>
2023-09-18 02:58:29 +0530

    auth page steps complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home/Home';
 import Navigation from './components/shared/Navigation/Navigation'
+import Home from './pages/Home/Home';
+import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
