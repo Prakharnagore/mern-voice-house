a4d777480bfb3447c7c5e70529fa6ca675bc514e
Prakhar Nagore <prakharnagore111@gmail.com>
2023-09-19 01:37:25 +0530

    layout and routes change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navigation from './components/shared/Navigation/Navigation'
 import Home from './pages/Home/Home';
-import Register from './pages/Register/Register';
-import Login from './pages/Login/Login';
+import Authenticate from "./pages/Authenticate/Authenticate"
+import Activate from "./pages/Activate/Activate"
+import Rooms from "./pages/Rooms/Rooms"
+// import Register from './pages/Register/Register';
+// import Login from './pages/Login/Login';
+
+const isAuth = false;
+const user = { activated: false }
 
 function App() {
   return (
     <BrowserRouter>
       <Navigation />
+      {/* Guest Route - If your is not logged in */}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={isAuth ? <Navigate to={{ pathname: '/rooms' }} /> : <Home />} />
+      </Routes>
+      <Routes>
+        <Route path="/authenticate" element={isAuth ? <Navigate to={{ pathname: '/rooms' }} /> : <Authenticate />} />
+      </Routes>
+      {/* Semi Protected Route - If user is logged in but do not have activated account */}
+      <Routes>
+        <Route path="/activate" element={!isAuth ? <Navigate to={{ pathname: '/', }} /> : isAuth && !user.activated ? <Activate /> : <Navigate to={{ pathname: '/rooms' }} />} />
+      </Routes>
+      {/* Protected Route - user in logged in and has activated account */}
+      <Routes>
+        <Route path="/rooms" element={!isAuth ? <Navigate to={{ pathname: '/' }} /> : isAuth && !user.activated ? <Navigate to={{ pathname: '/activate' }} /> : <Rooms />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import styles from './Home.module.css';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Card from '../../components/shared/Card/Card';
 import Button from '../../components/shared/Button/Button';
 
 const Home = () => {
-    const signInLinkStyle = {
-        color: '#0077ff',
-        fontWeight: 'bold',
-        textDecoration: 'none',
-        marginLeft: '10px',
-    };
+    // const signInLinkStyle = {
+    //     color: '#0077ff',
+    //     fontWeight: 'bold',
+    //     textDecoration: 'none',
+    //     marginLeft: '10px',
+    // };
 
     const navigate = useNavigate();
 
     function startRegister() {
-        navigate('/register');
+        navigate('/authenticate');
     }
 
     return (
@@ -27,15 +27,12 @@ const Home = () => {
                     gradually to make sure nothing breaks
                 </p>
                 <div>
-                    <Button onClick={startRegister} text="Get your username" />
+                    <Button onClick={startRegister} text="Let's go" />
                 </div>
                 <div className={styles.signinWrapper}>
                     <span className={styles.hasInvite}>
                         Have an invite text?
                     </span>
-                    <Link style={signInLinkStyle} to="/login">
-                        Sign in
-                    </Link>
                 </div>
             </Card>
         </div>
